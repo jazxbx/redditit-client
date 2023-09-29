609cead4c8500f47bd8cd893ccca39b5e4e9b1b4
jazz <jazxbx@gmail.com>
2023-09-29 15:39:25 -0700

    add votes to subreddit display

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,5 +1,83 @@
-function Votes() {
-  return <div>Votes</div>;
+import { useOutletContext } from "react-router-dom";
+import { ArrowUpward, ArrowDownward } from "@mui/icons-material";
+import { API } from "../lib";
+
+export function Votes({ post }) {
+  const { token, fetchPosts } = useOutletContext();
+
+  async function handleUpVote() {
+    const res = await fetch(`${API}/votes/upvotes/${post.id}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    fetchPosts();
+    const info = await res.json();
+    if (!info.success) {
+      deleteUpVote();
+    }
+  }
+
+  async function handleDownVote() {
+    const res = await fetch(`${API}/votes/downvotes/${post.id}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    fetchPosts();
+    const info = await res.json();
+    if (!info.success) {
+      deleteDownVote();
+    }
+  }
+
+  async function deleteUpVote() {
+    const res = await fetch(`${API}/votes/upvotes/${post.id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    fetchPosts();
+    const info = await res.json();
+  }
+
+  async function deleteDownVote() {
+    const res = await fetch(`${API}/votes/downvotes/${post.id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    fetchPosts();
+    const info = await res.json();
+  }
+
+  return !token ? (
+    <></>
+  ) : (
+    <>
+      <button
+        onClick={() => {
+          handleUpVote();
+          deleteDownVote();
+        }}
+      >
+        <ArrowUpward />
+      </button>
+      {post.upvotes.length - post.downvotes.length}
+      <button
+        onClick={() => {
+          handleDownVote();
+          deleteUpVote();
+        }}
+      >
+        <ArrowDownward />
+      </button>
+    </>
+  );
 }
 
 export default Votes;
--- src/routes/subreddit.jsx
+++ src/routes/subreddit.jsx
@@ -1,5 +1,7 @@
-import { Link, useOutletContext, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Button from "../components/Button";
+import { useOutletContext } from "react-router-dom";
+import Votes from "../components/Votes";
 
 function Subreddit() {
   const { subreddits, posts } = useOutletContext();
@@ -18,20 +20,30 @@ function Subreddit() {
       <div>
         {filteredPosts.length === 0 ? (
           <div>
-            <p>Subredditit empty, create a post!</p>
+            <p>Subreddit empty, create a post!</p>
             <Link to={"/submit"}>
               <Button>Post</Button>
             </Link>
           </div>
         ) : (
-          filteredPosts.map((post) => (
-            <div key={post.id} className="border-2 border-indigo-500/75 p-7">
-              <h2>r/{post.subreddit.name}</h2>
-              <p className="text-sm">u/{post.user.username}</p>
-              <h1 className="my-3 text-lg font-semibold">{post.title}</h1>
-              <p>{post.text}</p>
-            </div>
-          ))
+          <div>
+            <h1 className="text-4xl">r/{findSubreddit.name}</h1>
+            {filteredPosts.map((post) => (
+              <div
+                key={post.id}
+                className="grid border-2 border-indigo-500/75 p-7"
+              >
+                <div>
+                  <p className="text-sm">Posted by u/{post.user.username}</p>
+                  <h1 className="my-3 text-lg font-semibold">{post.title}</h1>
+                  <p>{post.text}</p>
+                </div>
+                <div>
+                  <Votes post={post} />
+                </div>
+              </div>
+            ))}
+          </div>
         )}
       </div>
     </div>
@@ -39,10 +51,3 @@ function Subreddit() {
 }
 
 export default Subreddit;
-
-// function Subreddit() {
-
-//   return <div>subreddit</div>;
-// }
-
-// export default Subreddit;
